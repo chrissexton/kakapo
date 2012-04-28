4128018fc9c365657d66a80e7461520f108ffa25
Issac Trotts <issac.trotts@gmail.com>
2012-04-28 18:55:26 +0000

    Add comment

--- testing.lisp
+++ testing.lisp
@@ -5,6 +5,8 @@
     (lambda (s)
       (define -section s)))
 
+  ;; T' tests its argument. If the argument evaluates to true then nothing
+  ;; happens. Otherwise an exception is thrown.
   (define T'
     (lambda (b)
       (if b nil

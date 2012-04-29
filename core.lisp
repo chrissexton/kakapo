1193a00c9be7d56dceec790c8545d523250f3479
Issac Trotts <issac.trotts@gmail.com>
2012-04-28 17:44:05 -0700

    Add map and identity functions.

--- core.lisp
+++ core.lisp
@@ -5,3 +5,13 @@
     0
     (+ 1 (len (cdr ls))))))
 
+(define map
+  (lambda (f ls)
+    (if (equal? ls '())
+      '()
+      (cons (f (car ls))
+            (map f (cdr ls))))))
+
+(define identity
+  (lambda (x) x))
+
--- test/list.lisp
+++ test/list.lisp
@@ -9,3 +9,8 @@
 (T' (= 1 (len '(a))))
 (T' (= 2 (len '(a b))))
 
+(S' "map")
+(T' (equal? '() (map identity '())))
+(T' (equal? '(1) (map identity '(1))))
+(T' (equal? '(1 2) (map identity '(1 2))))
+
